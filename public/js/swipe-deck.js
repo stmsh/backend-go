dc3cfa309c07aeb3051cba2a3a23e8a1122d3fb5
Stanislau Tur <stanislau.tur@gmail.com>
2024-06-11 23:11:59 +0200

    add swipe indicators

--- public/js/swipe-deck.js
+++ public/js/swipe-deck.js
@@ -30,6 +30,92 @@ class SwipeDeck extends HTMLElement {
         const children = Array.from(this.children);
         children.forEach((child) => this.setup(child));
         document.addEventListener("pointermove", this.handleMove);
+
+        const styles = document.createElement("style");
+        styles.innerHTML = `
+.swipe_deck__item {
+    --swipe-deck-dx: 0px;
+    --swipe-deck-angle: 0deg;
+    --swipe-deck-progress: 0;
+
+    cursor: grab;
+    user-select: none;
+    transform-origin: center;
+    touch-action: none;
+    transform: translateX(var(--swipe-deck-dx)) rotate(var(--swipe-deck-angle));
+}
+
+.swipe_deck__item--dragging {
+    cursor: grabbing;
+    transition: none;
+}
+
+.swipe_deck__item--releasing {
+    transition: transform .1s;
+}
+
+.swipe_deck__item--left::after {
+    content: '👎';
+    font-size: 4rem;
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+
+    display: block;
+    width: 6rem;
+    height: 6rem;
+    border-radius: 50%;
+    line-height: 7rem;
+    text-align: center;
+
+    background-image:   
+        conic-gradient(transparent calc(100% - var(--swipe-deck-progress) * 100%), white 0);
+}
+
+.swipe_deck__item--right::after {
+    content: '👍';
+    font-size: 4rem;
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+
+    display: block;
+    width: 6rem;
+    height: 6rem;
+    border-radius: 50%;
+    text-align: center;
+
+    background-image: 
+        conic-gradient(white calc(var(--swipe-deck-progress) * 100%), transparent 0);
+}
+
+.swipe_deck__item--left::before {
+    position: absolute;
+    display: block;
+    content: '';
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #f87171;
+    opacity: calc(var(--swipe-deck-progress) * 0.8);
+    border-radius: inherit;
+}
+
+.swipe_deck__item--right::before {
+    position: absolute;
+    display: block;
+    content: '';
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #4ade80;
+    opacity: calc(var(--swipe-deck-progress) * 0.8);
+    border-radius: inherit;
+}
+`;
+        document.head.insertAdjacentElement("beforeend", styles);
     }
 
     disconnectedCallback() {
@@ -40,10 +126,7 @@ class SwipeDeck extends HTMLElement {
 
     /** @param {HTMLElement} el */
     setup(el) {
-        el.style.cursor = "grab";
-        el.style.userSelect = "none";
-        el.style.transformOrigin = "center";
-        el.style.touchAction = "none";
+        el.classList.add("swipe_deck__item");
         el.addEventListener("pointerdown", this.handleMoveStart);
     }
 
@@ -57,8 +140,7 @@ class SwipeDeck extends HTMLElement {
         e.stopPropagation();
 
         this.focusedElement = e.currentTarget;
-        this.focusedElement.style.cursor = "grabbing";
-        this.focusedElement.style.transition = "";
+        this.focusedElement.classList.add("swipe_deck__item--dragging");
         this.startX = e.clientX;
         this.startY = e.clientY;
 
@@ -74,34 +156,53 @@ class SwipeDeck extends HTMLElement {
         }
         e.stopPropagation();
 
-        const swipeDistance = this.getSwipeDistance();
-        const dx = clamp(
-            -swipeDistance,
-            e.clientX - this.startX,
-            swipeDistance,
-        );
-        const swipeRatio = dx / swipeDistance;
-        const angle = swipeRatio * DEFAULT_ANGLE;
+        const distance = this.getSwipeDistance();
+        const dx = clamp(-distance, e.clientX - this.startX, distance);
+        const progress = Math.abs(dx / distance);
+        const angle = Math.sign(dx) * progress * DEFAULT_ANGLE;
 
-        this.focusedElement.style.transform = `translateX(${dx}px) rotate(${angle}deg)`;
+        this.focusedElement.style.setProperty("--swipe-deck-dx", dx + "px");
+        this.focusedElement.style.setProperty(
+            "--swipe-deck-progress",
+            progress,
+        );
+        this.focusedElement.style.setProperty(
+            "--swipe-deck-angle",
+            angle + "deg",
+        );
+        this.focusedElement.classList.add("swipe_deck__item--dragging");
+
+        if (dx > 0) {
+            this.focusedElement.classList.remove("swipe_deck__item--left");
+            this.focusedElement.classList.add("swipe_deck__item--right");
+        } else {
+            this.focusedElement.classList.add("swipe_deck__item--left");
+            this.focusedElement.classList.remove("swipe_deck__item--right");
+        }
     };
 
     /** @param {PointerEvent} e */
     handleRelease = (e) => {
         e.stopPropagation();
         const target = this.focusedElement;
-        target.style.cursor = "grab";
         this.focusedElement = null;
+
+        target.classList.remove("swipe_deck__item--dragging");
 
         const moveDistance = e.clientX - this.startX;
         if (Math.abs(moveDistance) < this.getSwipeDistance()) {
-            target.style.transition = "transform .1s";
-            target.style.transform = "";
+            target.style.removeProperty("--swipe-deck-dx");
+            target.style.removeProperty("--swipe-deck-angle");
+            target.style.removeProperty("--swipe-deck-progress");
+            target.classList.remove(
+                "swipe_deck__item--left",
+                "swipe_deck__item--right",
+            );
+            target.classList.add("swipe_deck__item--releasing");
+
             target.addEventListener(
                 "transitionend",
-                () => {
-                    target.style.transition = "";
-                },
+                () => target.classList.remove("swipe_deck__item--releasing"),
                 { once: true },
             );
             return;
